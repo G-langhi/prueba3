7e221752fd3cf2c722d1f52bf871d584220be874
Guille-langhi <gmlgnr82@gmail.com>
2025-03-29 09:11:53 -0300

    se agregaron comentarios a ejercicio1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,4 +1,8 @@
+#se define variable nombre
 nombre = "Guillermo"
+#se define variable apellido
 apellido = "Langhi"
+#se define variable materia
 materia = "programación"
+#se imprime mensaje por consola
 print(f"Hola soy {nombre} {apellido} alumno de {materia}")
